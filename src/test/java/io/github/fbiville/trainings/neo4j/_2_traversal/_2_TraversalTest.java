ae41734c25375ccf467e80668cf444aab0e9ec2c
@fbiville <florent.biville@gmail.com>
2016-11-29 01:20:43 +0100

    Add solutions to traversal exercises

--- src/test/java/io/github/fbiville/trainings/neo4j/_2_traversal/_2_TraversalTest.java
+++ src/test/java/io/github/fbiville/trainings/neo4j/_2_traversal/_2_TraversalTest.java
@@ -4,15 +4,21 @@ import io.github.fbiville.trainings.neo4j.internal.GraphTests;
 import io.github.fbiville.trainings.neo4j.internal.MonkeyIslandGraph;
 import org.junit.Before;
 import org.junit.Test;
+import org.neo4j.graphalgo.GraphAlgoFactory;
 import org.neo4j.graphalgo.PathFinder;
 import org.neo4j.graphdb.Label;
 import org.neo4j.graphdb.Node;
 import org.neo4j.graphdb.Path;
+import org.neo4j.graphdb.PathExpanders;
+import org.neo4j.graphdb.RelationshipType;
 import org.neo4j.graphdb.Transaction;
+import org.neo4j.graphdb.traversal.BranchOrderingPolicies;
+import org.neo4j.graphdb.traversal.Evaluators;
 import org.neo4j.graphdb.traversal.Traverser;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.assertj.core.api.Assertions.fail;
+import static org.neo4j.graphalgo.GraphAlgoFactory.shortestPath;
 
 public class _2_TraversalTest extends GraphTests {
 
@@ -25,17 +31,16 @@ public class _2_TraversalTest extends GraphTests {
     public void should_traverse_hate_relationships() {
         try (Transaction ignored = graphDb.beginTx()) {
             Node elaine = graphDb.findNode(Label.label("Character"), "name", "Elaine Marley");
-             /*
-              * TODO: start a graph from Elaine to find who she (transitively) hates
-              * HINT: You don't need to write a custom evaluator!
-              */
-            Iterable<Node> nodes = null;
-            fail("You should write a traversal to know who Elaine transitively hates");
+            Iterable<Node> nodes = graphDb.traversalDescription()
+                    .relationships(RelationshipType.withName("HATES"))
+                    .evaluator(Evaluators.excludeStartPosition())
+                    .traverse(elaine)
+                    .nodes();
 
             assertThat(nodes)
-                .hasSize(3)
-                .extracting(node -> node.getProperty("name"))
-                .containsOnly("Guybrush Threepwood", "LeChuck", "Largo LaGrande");
+                    .hasSize(3)
+                    .extracting(node -> node.getProperty("name"))
+                    .containsOnly("Guybrush Threepwood", "LeChuck", "Largo LaGrande");
         }
     }
 
@@ -43,15 +48,13 @@ public class _2_TraversalTest extends GraphTests {
     public void should_determine_whether_florent_and_guybrush_are_connected() {
         try (Transaction ignored = graphDb.beginTx()) {
 
-            Node florent = null/*TODO*/;
-            Node guybrush = null /*TODO*/;
+            Node florent = graphDb.findNode(Label.label("Intruder"), "name", "Florent Biville");
+            Node guybrush = graphDb.findNode(Label.label("Character"), "name", "Guybrush Threepwood");
 
             Traverser traverser = graphDb.bidirectionalTraversalDescription()
-                // TODO: describe unidirectional traversals for both sides
-                // HINT: it's VERY simple
-                .traverse(florent, guybrush);
-
-            fail("You should write a bidirectional traversal");
+                    .startSide(graphDb.traversalDescription())
+                    .endSide(graphDb.traversalDescription())
+                    .traverse(florent, guybrush);
 
             assertThat(traverser.nodes()).isEmpty();
         }
@@ -60,10 +63,9 @@ public class _2_TraversalTest extends GraphTests {
     @Test
     public void should_use_shortest_path_algorithms_to_find_the_same_result() {
         try (Transaction ignored = graphDb.beginTx()) {
-            Node florent = null/* TODO */;
-            Node guybrush = null/*TODO*/;
-            PathFinder<Path> paths = null/*TODO (you don't have to reimplement an algorithm yourself!) */;
-            fail("You should use the implemented shortest path algorithm");
+            Node florent = graphDb.findNode(Label.label("Intruder"), "name", "Florent Biville");
+            Node guybrush = graphDb.findNode(Label.label("Character"), "name", "Guybrush Threepwood");
+            PathFinder<Path> paths = shortestPath(PathExpanders.allTypesAndDirections(), 42);
 
             assertThat(paths.findSinglePath(florent, guybrush)).isNullOrEmpty();
         }
